a6d8b51dd2b0561002e3d9f9af1a02ddc5f67fcf
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2023-06-01 13:54:49 -0300

    enums

---
+++ CBro/enums.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Weekday {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
